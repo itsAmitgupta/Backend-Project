01296adb80792cdc5951a9a1d2b29dfcf5a2a5c8
Amit Gupta <guptaamitgupta819@gmail.com>
2024-03-10 12:10:22 +0530

    minor error fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 // require('dotenv').config({path:'./env'})
-import dotenv from 'dotenv'
-import connectDb from './db/connect.js'
-import { app } from './app.js'
-
+import dotenv from "dotenv";
+import connectDb from "./db/connect.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./.env",
+});
 connectDb()
-.then(app.listen(process.env.PORT || 8000) ,()=>{
-    console.log(`Database Connected on port ${process.env.PORT}`)
-})
-.catch((err)=>{
-    console.log(`Database Connection error`,err)
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Database Connected on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Database Connection error`, err);
+  });
