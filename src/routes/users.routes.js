60d62958aaecbf86d69134330ddd75e783f8fab2
Amit Gupta <guptaamitgupta819@gmail.com>
2024-03-14 12:14:38 +0530

    Addrefresh token endpoint

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/users.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/users.controller.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -19,6 +19,9 @@ router.route("/register").post(
     )
 
 router.route("/login").post(loginUser)
+
+//secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
